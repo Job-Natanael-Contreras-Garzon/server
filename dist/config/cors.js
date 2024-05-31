8c1465d08d43ad259b27d0a32f5ac581a079de3f
Job-Natanael-Contreras-Garzón <contrerasjob123@gmail.com>
2024-05-30 22:24:35 -0400

    remastered

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -5,9 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/config/cors.ts
 const cors_1 = __importDefault(require("cors"));
-const allowedOrigins = ['https://proyectocruz.vercel.app'];
 const corsOptions = {
-    origin: allowedOrigins,
-    optionsSuccessStatus: 200,
+    origin: 'https://proyectocruz.vercel.app',
+    optionsSuccessStatus: 200
 };
 exports.default = (0, cors_1.default)(corsOptions);
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,11 +1,9 @@
 // src/config/cors.ts
 import cors from 'cors';
 
-const allowedOrigins = ['https://proyectocruz.vercel.app'];
-
-const corsOptions: cors.CorsOptions = {
-    origin: allowedOrigins,
-    optionsSuccessStatus: 200,
+const corsOptions = {
+  origin: 'https://proyectocruz.vercel.app',
+  optionsSuccessStatus: 200
 };
 
 export default cors(corsOptions);
