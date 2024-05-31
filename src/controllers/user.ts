d0f53436bd9e31e46c1ab5aeaf843190263d9dab
Job-Natanael-Contreras-Garzón <contrerasjob123@gmail.com>
2024-05-31 02:46:09 -0400

    arrcontroller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,121 +1,118 @@
 import { Request, Response } from 'express';
-import bycrypt from 'bcryptjs';
-import { User, callActualizarPassword, obtener_categoria_permiso } from '../models/User';
-import jwt from 'jsonwebtoken'
-import { callCrearUsuarioProcedure } from '../models/User';
+import bcrypt from 'bcryptjs';
+import { User, callActualizarPassword, obtener_categoria_permiso, callCrearUsuarioProcedure } from '../models/User';
+import jwt from 'jsonwebtoken';
 
 export const newUser = async (req: Request, res: Response) => {
-    
     const { nombreAdministrador, telefono, correoElectronico, username, password, tipoPermiso } = req.body;
 
-    //codificacion de la contraseña
-    const hashedPassword = await bycrypt.hash(password,10)
+    if (!username || !password) {
+        return res.status(400).json({
+            msg: 'El nombre de usuario y la contraseña son obligatorios.'
+        });
+    }
 
-    //validar si el Usuario ya existe en la Base de Datos
-    const user = await User.findOne({where: { username: username }})
-    if(user){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.findOne({ where: { username: username } });
+    if (user) {
         return res.status(400).json({
             msg: `Ya existe un usuario con el nombre ${username}`
-        })
+        });
     }
 
-
-    
     try {
-        //Guardar Usuario en la base de datos
-        // await User.create({
-        //     username: username,
-        //     password: hashedPassword
-        // })
-        //console.log(nombreAdministrador);
         await callCrearUsuarioProcedure(nombreAdministrador, telefono, correoElectronico, username, hashedPassword, tipoPermiso);
-
         res.json({
             msg: `Usuario ${username} creado exitosamente`,
-        })
+        });
     } catch (error) {
         res.status(400).json({
-            msg: 'Ups Ocurrio Un error',
+            msg: 'Ups, ocurrió un error',
             error
-        })
+        });
+    }
+};
+
+export const newPassword = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({
+            msg: 'El nombre de usuario y la nueva contraseña son obligatorios.'
+        });
     }
 
-}
-
-export const newPassword = async (req: Request, res: Response) =>{
-    const {username, password} = req.body;
-    //Encriptamos el password
-    const hashedPassword = await bycrypt.hash(password,10)
-    //validamos si el usuario existe en la base
-    const user: any = await User.findOne({where: { username: username }});
-    if(!user){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user: any = await User.findOne({ where: { username: username } });
+    if (!user) {
         return res.status(400).json({
             msg: `No existe un usuario con el nombre ${username} en la base de datos`
-        })
+        });
     }
 
     try {
         await callActualizarPassword(username, hashedPassword);
-
         res.json({
             msg: `Password actualizado exitosamente`,
-        })
+        });
     } catch (error) {
         res.status(400).json({
-            msg: 'Ups Ocurrio Un error',
+            msg: 'Ups, ocurrió un error',
             error
-        })
+        });
     }
-
-}
+};
 
 export const loginUser = async (req: Request, res: Response) => {
-    
-    const {username, password} = req.body;
-    //validamos si el usuario existe en la base
-    const user: any = await User.findOne({where: { username: username }});
-    if(!user){
+    const { username, password } = req.body;
+
+    if (!username || !password) {
         return res.status(400).json({
-            msg: `No existe un usuario con el nombre ${username} en la base de datos`
-        })
+            msg: 'El nombre de usuario y la contraseña son obligatorios.'
+        });
     }
 
-    //validamos el password
-    const passwordValido = await bycrypt.compare(password,user.password);
-    if(!passwordValido){
+    const user: any = await User.findOne({ where: { username: username } });
+    if (!user) {
+        return res.status(400).json({
+            msg: `No existe un usuario con el nombre ${username} en la base de datos`
+        });
+    }
+
+    const passwordValido = await bcrypt.compare(password, user.password);
+    if (!passwordValido) {
         return res.status(400).json({
             msg: 'Password Incorrecta'
-        })
+        });
     }
 
-    //generar token
-    const token = jwt.sign({
-                    username: username
-                  }, process.env.SECRET_KEY || 'SuperPutz')
-
-
-    res.json(token)
-
-}
+    const token = jwt.sign({ username: username }, process.env.SECRET_KEY || 'SuperPutz');
+    res.json(token);
+};
 
 export const UserPer = async (req: Request, res: Response) => {
-    const {username} = req.body;
-    //validamos si el usuario existe en la base
-    const user: any = await User.findOne({where: { username: username }});
-    if(!user){
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).json({
+            msg: 'El nombre de usuario es obligatorio.'
+        });
+    }
+
+    const user: any = await User.findOne({ where: { username: username } });
+    if (!user) {
         return res.status(400).json({
             msg: `No existe un usuario con el nombre ${username} en la base de datos`
-        })
+        });
     }
 
     try {
-        const permiso=await obtener_categoria_permiso(username);
-
-        res.json(permiso)
+        const permiso = await obtener_categoria_permiso(username);
+        res.json(permiso);
     } catch (error) {
         res.status(400).json({
-            msg: 'Ups Ocurrio Un error',
+            msg: 'Ups, ocurrió un error',
             error
-        })
+        });
     }
-}
+};
