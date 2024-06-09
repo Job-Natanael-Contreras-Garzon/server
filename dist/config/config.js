a39163a2b302ed6409fcb5ae6c9788bb62f09215
Job-Natanael-Contreras-Garzón <contrerasjob123@gmail.com>
2024-06-09 16:46:28 -0400

    update dist

---
+++ dist/config/config.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const config = {
+    development: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
+    production: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
+};
+exports.default = config;
+//# sourceMappingURL=config.js.map
